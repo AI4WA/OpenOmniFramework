9f760601b61f8072e326aa702afd66740364f65b
PascalSun <pascalsun23@gmail.com>
2024-04-02 12:50:42 +0800

    add job.sh

---
+++ Client/Worker/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=llm
+#SBATCH --output=result.txt
+#SBATCH --nodes=1
+#SBATCH --time=03:00:00
+#SBATCH --partition=gpu
+
+module load cuda/11.6
+
+cd $MYGROUP/Jarv5/Client/Worker/
+
+source venv/bin/activate
+python3 main.py --token f07d79cda0137abacd39258654090410650c2c0d

45fa01419e6515abc62f12556e0ff777db81759b
PascalSun <pascalsun23@gmail.com>
2024-04-04 11:15:27 +0800

    success

--- Web/app/tasks/page.tsx
+++ Web/app/tasks/page.tsx
@@ -56,7 +56,7 @@ subscription OnStarted {
 
 const TASK_SUCCESS = gql`
 subscription OnSuccess {
-    success: worker_task_aggregate(where: {result_status: {_eq: "success"}}) {
+    success: worker_task_aggregate(where: {result_status: {_eq: "completed"}}) {
         aggregate {
             count
         }
@@ -111,47 +111,6 @@ const TaskPage = () => {
     if (error) return <div className="text-red-500">Error :(</div>;
 
     return (
-        // <div className="m-4">
-        //
-        //     <div className="shadow-xs p-4 bg-white">
-        //         <table className="min-w-full divide-y divide-gray-200 table-auto">
-        //             <thead className="bg-gray-50">
-        //             <tr>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-        //                 {/*<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>*/}
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Prompt</th>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Task</th>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User
-        //                 </th>
-        //                 {/*<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Created*/}
-        //                 {/*    At*/}
-        //                 {/*</th>*/}
-        //                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Updated
-        //                     At
-        //                 </th>
-        //             </tr>
-        //             </thead>
-        //             <tbody className="bg-white divide-y divide-gray-200">
-        //             {data && data?.worker_task.map((task: Task, index: number) => (
-        //                 <tr key={index}>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.id}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.name}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.result_status}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{JSON.stringify(task.parameters?.prompt)}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{JSON.stringify(task.parameters?.llm_task_type)}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.work_type}</td>
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.authenticate_user.username}</td>
-        //                     {/*<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.created_at}</td>*/}
-        //                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{task.updated_at}</td>
-        //                 </tr>
-        //             ))}
-        //             </tbody>
-        //         </table>
-        //     </div>
-        // </div>
         <div className="container mx-auto px-4 py-8">
             {/* Cards for task states */}
             <div className="grid grid-cols-4 gap-4 mb-8">

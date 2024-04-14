5da4fc7cf219fed33bcf6ec1ba66b30b45b3f340
PascalSun <pascalsun23@gmail.com>
2024-04-14 11:18:15 +0800

    update the frontedn

--- Web/app/jarv5/Text2Speech.tsx
+++ Web/app/jarv5/Text2Speech.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, {useState} from "react";
 import {useSubscription, gql} from "@apollo/client";
 import {
     Typography,
@@ -7,10 +7,11 @@ import {
     Box,
     List,
     ListItem,
-    Divider,
+    Divider, IconButton,
 } from "@mui/material";
 import moment from "moment";
 import Waveform from "@/app/jarv5/Waveform";
+import {Refresh} from '@mui/icons-material';
 
 
 interface Text2SpeechProps {
@@ -36,18 +37,36 @@ subscription Text2SpeechSubscription($homeId: bigint!) {
 `
 
 const Text2Speech: React.FC<Text2SpeechProps> = ({homeId}) => {
-
+    const [refreshTTSKey, setRefreshTTSKey] = useState<number>(0);
     const {data: speech2textData} = useSubscription(Text2Speech_SUBSCRIPTION, {
         variables: {
             homeId: homeId
         }
     });
+    const handleTTSRefresh = () => {
+        // This increments the key causing the component to re-render
+        setRefreshTTSKey(oldKey => oldKey + 1);
+    };
 
     return (
-        <Box sx={{padding: 0}}>
-            <Typography variant="h6" gutterBottom>
-                Jarv5 Response
-            </Typography>
+        <Box sx={{padding: 0}} key={refreshTTSKey}>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                padding: 0
+            }}>
+                <Typography variant="h6" gutterBottom>
+                    Jarv5 Response
+                </Typography>
+                <IconButton onClick={handleTTSRefresh} aria-label="refresh"
+                            size="small"
+                            sx={{padding: 0}}
+                >
+                    <Refresh/>
+                </IconButton>
+            </Box>
             <Paper elevation={1} sx={{padding: 1, border: 'none', boxShadow: 'none', overflow: "auto", height: "32vh"}}>
                 {speech2textData?.hardware_text2speech.map((speech: Text2SpeechData) => (
                     <div key={speech.id}>

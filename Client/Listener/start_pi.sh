f0578bb94f9e254c3ef63c640aca761aa8f2da96
PascalSun <pascalsun23@gmail.com>
2024-04-16 11:58:02 +0800

    change scripts

---
+++ Client/Listener/start_pi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export DISPLAY=:0.0
+
+# Sync files process
+nohup python3 sync_files.py --dest_ip 146.118.70.154 --dest_directory /home/pascal/Assistant/Client/Listener/data --dest_username pascal > sync_files.log 2>&1 &
+
+# Sync to S3 process
+nohup python3 sync_to_s3.py --home_id 1 > sync_to_s3.log 2>&1 &
+
+# Videos acquisition process
+nohup python3 videos_acquire.py --token 60a00917dbfb3fe21ebbeff8dc95de0f5ffba9a5 --home_id 1 --api_domain https://api.nlp-tlp.org > videos_acquire.log 2>&1 &
+
+# Audios acquisition process
+nohup python3 audios_acquire.py --token 60a00917dbfb3fe21ebbeff8dc95de0f5ffba9a5 --home_id 1 --api_domain https://api.nlp-tlp.org --default_microphone "USB Device 0x46d:0x825: Audio (hw:4,0)" > audios_acquire.log 2>&1 &

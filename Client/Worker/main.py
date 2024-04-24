3ecfff8323e4c26e4f0e99fbbd33191d65504c37
moebuta <wenxiao.zhang@research.uwa.edu.au>
2024-04-24 10:50:38 +0800

    fix formatting

--- Client/Worker/main.py
+++ Client/Worker/main.py
@@ -169,7 +169,9 @@ def handle_chat(task: dict, api: API):
     with timer(logger=logger, message="run_chat_task"):
         llm_adaptor = LLMAdaptor(llm_model)
         # if prompt is not None, then we are responding
-        res = llm_adaptor.create_chat_completion(prompt=prompt, messages=messages, handle_chat=True)
+        res = llm_adaptor.create_chat_completion(
+            prompt=prompt, messages=messages, handle_chat=True
+        )
         logger.info(res)
         if prompt is not None:
             api.post_chat_summary(

bdf54bd886b66b570bf348735a0a19d4abf0c672
PascalSun <pascalsun23@gmail.com>
2024-07-29 18:13:59 +0800

    update example code

--- Client/Listener/start_pi.sh
+++ Client/Listener/start_pi.sh
@@ -10,4 +10,4 @@ nohup  python3 storage.py  --token c284262e850b8ac98e9174db620dc246ea6d9043 --ap
 nohup python3 videos_acquire.py --token c284262e850b8ac98e9174db620dc246ea6d9043 --api_domain http://192.168.50.113:8000 > videos_acquire.log 2>&1 &
 
 # Audios acquisition process
-nohup python3 audios_acquire.py --token c284262e850b8ac98e9174db620dc246ea6d9043 --track_cluster CLUSTER_GPT_4O_ETE_CONVERSATION --api_domain http://192.168.50.113:8000 --default_microphone "USB Device 0x46d:0x825" > audios_acquire.log 2>&1 &
+nohup python3 audios_acquire.py --token c284262e850b8ac98e9174db620dc246ea6d9043 --track_cluster CLUSTER_Q_ETE_CONVERSATION --api_domain http://192.168.50.113:8000 --default_microphone "USB Device 0x46d:0x825" > audios_acquire.log 2>&1 &
---
+++ Client/Responder/start_pi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+python3 play_speech.py  --token c284262e850b8ac98e9174db620dc246ea6d9043 --api_domain http://192.168.50.113:8000

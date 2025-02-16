d7dfaa13da7e7eeca62fcde3d69c3449dbe9ffdd
Wenxiao <wenxiao.zhang@research.uwa.edu.au>
2025-02-16 22:39:25 +0800

    fix moviepy version issue

--- API/hardware/views.py
+++ API/hardware/views.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from django.conf import settings
 from django.http import HttpResponse
 from drf_yasg.utils import swagger_auto_schema
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy import VideoFileClip, concatenate_videoclips
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
